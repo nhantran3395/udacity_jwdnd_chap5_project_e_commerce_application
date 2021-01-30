8d421cff0d61b88e24c9d508c45a6173bdbde777
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-01-30 08:20:45 +0700

    add all arg constructor

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -1,11 +1,13 @@
 package com.example.demo.model.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
 public class CreateUserRequest {
 
 	@NotBlank
