5db4c86758e7469ceb6487021f7144204000cce3
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-01-31 15:17:19 +0700

    add constraint to modify cart request

--- src/main/java/com/example/demo/model/requests/ModifyCartRequest.java
+++ src/main/java/com/example/demo/model/requests/ModifyCartRequest.java
@@ -5,17 +5,24 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @ToString
 public class ModifyCartRequest {
 	
 	@JsonProperty
+	@NotBlank
+	@Email
 	private String username;
 	
 	@JsonProperty
+	@NotBlank
 	private long itemId;
 	
 	@JsonProperty
+	@NotBlank
 	private int quantity;
 }
