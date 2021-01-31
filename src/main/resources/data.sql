6ccfa2208025fdc981bbf7dc8007b5a36ad9d13d
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-01-31 14:29:26 +0700

    add watches as item for realness

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
-insert into item (name, price, description) values ('Round Widget', 2.99, 'A widget that is round');
-insert into item (name, price, description) values ('Square Widget', 1.99, 'A widget that is square');
+insert into item (name, price, description)
+values
+('TISSOT SEASTAR 1000 CHRONOGRAPH', 525, 'The Tissot Seastar 1000 merges style and performance without compromising either.'),
+('TISSOT GENTLEMAN', 375, 'The Tissot Gentleman is a multi-purpose watch, both ergonomic and elegant in any circumstance.'),
+('TISSOT LE LOCLE POWERMATIC 80', 890, 'The name Le Locle seems to be a reliable ingredient of success.')
+;
